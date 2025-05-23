7210d4ea5db119cb8738df7c3ff913c96b7d815e
juneclyd <www.ramil241003@gmail.com>
2025-05-23 16:54:41 +0300

    feat: vite

---
+++ server2/client/src/App.jsx
@@ -0,0 +1,12 @@
+import TestComponent from "./components/TestComponent.jsx";
+
+function App() {
+
+    return (
+        <div className='App'>
+             <TestComponent />
+        </div>
+    );
+}
+
+export default App;
