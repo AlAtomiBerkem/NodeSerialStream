07cd8b3c80db5ebc54f30d8edaf08ab84f4c2714
juneclyd <www.ramil241003@gmail.com>
2025-05-28 14:41:35 +0300

    feat: add start menu

--- server2/client/src/App.jsx
+++ server2/client/src/App.jsx
@@ -2,14 +2,14 @@ import TestComponent from "./components/MainComponents.jsx";
 import ConnectionTab from "./components/ConnectionTab.jsx";
 import TestingDone from "./components/TestingDone.jsx"
 import ErrorComponent from "./components/ErrorComponent.jsx"
-import TestingStart from "./components/StartTesting/TestingStart.jsx"
+import TestingStart from "./components/TestingStart.jsx"
 import './reset.css' 
 
 function App() {
 
     return (
         <div className='App'>
-            <ErrorComponent />
+            <TestingStart />
          </div>
     );
 }
---
+++ server2/client/src/components/TestingStart.jsx
@@ -0,0 +1,69 @@
+import testStartBackdrop from '../UI/backdrops/testStartBackdrop.svg';
+
+import startButton from '../UI/startButtons/startActive.svg';
+import startButtonActive from '../UI/startButtons/startActiveSelect.svg';
+
+import BackButton from '../UI/bigBackButtons/bigBackBtnActiveSelect.svg';
+import BackButtonPushed from '../UI/bigBackButtons/bigBackBtnActiveEmpty.svg'
+
+import { useState } from 'react';
+
+export const TestingStart = () => {
+  const [isButtonPressed, setIsButtonPressed] = useState(false);
+  const [isBtnBack, setIsBtnBack] = useState(false);
+
+
+  return (
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        backgroundImage: `url(${testStartBackdrop})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        position: 'relative',
+      }}
+    >
+      <button 
+        onClick={() => setIsButtonPressed(!isButtonPressed)}
+        style={{
+          position: 'absolute',
+          top: '72.1%',
+          left: '50.1%',
+          transform: 'translate(-50%, -50%)',
+          background: 'transparent',
+          border: 'none',
+          cursor: 'pointer',
+          padding: 0
+        }}
+      >
+        <img 
+          src={isButtonPressed ? startButtonActive : startButton} 
+          alt="Start Button"
+        />
+      </button>
+
+      <button
+              onClick={() => setIsBtnBack(!isBtnBack)}
+        style={{
+          position: 'absolute',
+          top: '9%',
+          left: '10.3%',
+          transform: 'translate(-50%, -50%)',
+          background: 'transparent',
+          border: 'none',
+          cursor: 'pointer',
+          padding: 0
+        }}
+      >        
+        <img
+          src={isBtnBack ? BackButtonPushed : BackButton} 
+          alt="Start Button"
+       />
+      </button>
+    </div>
+  );
+};
+
+
+export default TestingStart;
