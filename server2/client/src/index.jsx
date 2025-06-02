c3d0c386243ba45c288f46f9b66b6c28a6f6eb5e
juneclyd <www.ramil241003@gmail.com>
2025-06-02 17:01:07 +0300

    fix/crash: react-routing add

--- server2/client/src/index.jsx
+++ server2/client/src/index.jsx
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'
-import { store } from './store/store.js'
+import App from './App.jsx';
+import { store } from './store/store.js';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
-  </Provider>,
-)
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+);
