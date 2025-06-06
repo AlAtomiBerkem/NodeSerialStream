e291d67923936274248becb09afa66cb930ab86e
juneclyd <www.ramil241003@gmail.com>
2025-06-06 10:13:24 +0300

    feat: stuct logick update

---
+++ server2/client/src/components/QuizComponents/imports.js
@@ -0,0 +1,14 @@
+// imports.js
+export { default as backdrop } from '../../UI/backdrops/qqq.png';
+export { default as Scale } from '../Scale/Scale.jsx';
+export { default as ButtonQuiz } from '../../helpers/ButtonQuiz.jsx';
+export { default as TrueCheckDone } from '../../UI/selectioAndMoveBtn/TryCheck.svg';
+export { default as TrueCheck } from '../../UI/selectioAndMoveBtn/TryUnCheck.svg';
+export { default as FakeCheck } from '../../UI/selectioAndMoveBtn/FakeUnCheck.svg';
+export { default as FakeCheckDone } from '../../UI/selectioAndMoveBtn/FakeCheck.svg';
+export { useButtonLogic } from './useButtonLogicl.js';
+export { getLeftBtnImage, getRightBtnImage } from './buttonUtils.js';
+export { default as QuestionWindow } from './QuestionWindow.jsx';
+export { default as QuizResults } from './QuizResults.jsx';
+export { default as WarningModal } from './WarningModal.jsx';
+export { QUESTIONS } from './Questions.js';
