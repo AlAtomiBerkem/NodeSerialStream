247083be3ef05702e3add34b6736c1ad71934393
juneclyd <www.ramil241003@gmail.com>
2025-06-06 15:22:37 +0300

    fix: btn file

--- server2/client/src/components/QuizComponents/buttonUtils.js
+++ server2/client/src/components/QuizComponents/buttonUtils.js
@@ -1,9 +1,11 @@
-import LbtnPushed from '../../UI/selectioAndMoveBtn/LbtnPushed.svg';
-import LbtnActive from '../../UI/selectioAndMoveBtn/LbtnActive.svg';
-import RbtnPushed from '../../UI/selectioAndMoveBtn/RbtnPushed.svg';
-import RbtnBlue from '../../UI/selectioAndMoveBtn/RbtnBlue.svg';
-import RbtnActive from '../../UI/selectioAndMoveBtn/RbtnActive.svg';
-import BlueBtnPushed from '../../UI/selectioAndMoveBtn/BlueBtnPushed.svg'
+import {
+  LbtnPushed,  
+  LbtnActive, 
+  RbtnPushed,
+  RbtnBlue,
+  RbtnActive, 
+  BlueBtnPushed,
+} from './imports.js'  
 
 export const getLeftBtnImage = (uiState) => {
   if (uiState.leftBtnState === 'pushed' || uiState.leftBtnClicked) {
