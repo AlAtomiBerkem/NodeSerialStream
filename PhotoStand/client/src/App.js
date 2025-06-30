d4e86c0b20c6645486ed238cf83c97610bad9cd3
juneclyd <www.ramil241003@gmail.com>
2025-06-30 13:56:20 +0300

    feat: add format Photostand

--- PhotoStand/client/src/App.js
+++ PhotoStand/client/src/App.js
@@ -1,41 +1,41 @@
-import { useState } from 'react';
-import BackgroundSelector from './components/BackgroundSelector';
-import PhotoCapture from './components/PhotoCapture';
-import WelcomeScreen from './components/WelcomeScreen';
-import './styles/App.css';
+import { useState } from "react";
+import BackgroundSelector from "./components/BackgroundSelector";
+import PhotoCapture from "./components/PhotoCapture";
+import WelcomeScreen from "./components/WelcomeScreen";
+import "./styles/App.css";
 
 function App() {
-    const [step, setStep] = useState('welcome'); // 'welcome' | 'background' | 'photo'
-    const [selectedBackground, setSelectedBackground] = useState(null);
-    const [backgroundId, setBackgroundId] = useState(null);
+  const [step, setStep] = useState("welcome"); // 'welcome' | 'background' | 'photo'
+  const [selectedBackground, setSelectedBackground] = useState(null);
+  const [backgroundId, setBackgroundId] = useState(null);
 
-    const handleBackgroundSelect = (url, id) => {
-        setSelectedBackground(url);
-        setBackgroundId(id);
-        setStep('photo');
-    };
+  const handleBackgroundSelect = (url, id) => {
+    setSelectedBackground(url);
+    setBackgroundId(id);
+    setStep("photo");
+  };
 
-    return (
-        <div className="app app-bg">
-            {step === 'welcome' && (
-                <WelcomeScreen onStart={() => setStep('background')} />
-            )}
-            {step === 'background' && !selectedBackground && (
-                <BackgroundSelector
-                    onSelect={(url, id) => handleBackgroundSelect(url, id)}
-                />
-            )}
-            {step === 'photo' && selectedBackground && (
-                <PhotoCapture
-                    backgroundId={backgroundId}
-                    onBack={() => {
-                        setSelectedBackground(null);
-                        setStep('background');
-                    }}
-                />
-            )}
-        </div>
-    );
+  return (
+    <div className="app app-bg">
+      {step === "welcome" && (
+        <WelcomeScreen onStart={() => setStep("background")} />
+      )}
+      {step === "background" && !selectedBackground && (
+        <BackgroundSelector
+          onSelect={(url, id) => handleBackgroundSelect(url, id)}
+        />
+      )}
+      {step === "photo" && selectedBackground && (
+        <PhotoCapture
+          backgroundId={backgroundId}
+          onBack={() => {
+            setSelectedBackground(null);
+            setStep("background");
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default App;
--- PhotoStand/client/src/components/BackgroundSelector.jsx
+++ PhotoStand/client/src/components/BackgroundSelector.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react';
-import '../styles/BackgroundSelector.css';
+import { useState } from "react";
+import "../styles/BackgroundSelector.css";
 
-import photoText from '../assets/select_fons/photo_text.png';
-import fonMask from '../assets/select_fons/fonMask.png';
-import selectBtn from '../assets/select_fons/selectBtn.png';
-import radioBtnActive from '../assets/select_fons/radioBtnActive.png';
-import radioBtn from '../assets/select_fons/radionBtn.png';
-import text  from '../assets/select_fons/text.png'
+import photoText from "../assets/select_fons/photo_text.png";
+import fonMask from "../assets/select_fons/fonMask.png";
+import selectBtn from "../assets/select_fons/selectBtn.png";
+import radioBtnActive from "../assets/select_fons/radioBtnActive.png";
+import radioBtn from "../assets/select_fons/radionBtn.png";
+import text from "../assets/select_fons/text.png";
 
-import background_1 from '../fons/image 1.png';
-import background_2 from '../fons/image 2.png';
-import background_3 from '../fons/image 3.png';
-import background_4 from '../fons/image 4.png';
-import background_5 from '../fons/image 5.png';
+import background_1 from "../fons/image 1.png";
+import background_2 from "../fons/image 2.png";
+import background_3 from "../fons/image 3.png";
+import background_4 from "../fons/image 4.png";
+import background_5 from "../fons/image 5.png";
 
 const backgrounds = [
   { id: 1, url: background_1 },
@@ -27,38 +27,41 @@ export default function BackgroundSelector({ onSelect }) {
 
   return (
     <>
-    <div className='conteiner-selecct'>
-      <img src={photoText} alt="Фото" className="background-selector-photo-text" />
-      <div className="background-selector-mask-wrapper">
-        <img src={fonMask} alt="Маска" className="background-selector-mask" />
-        <div className="background-selector-image-wrapper">
-          <img
-            src={backgrounds[currentIndex].url}
-            alt={`Фон ${currentIndex + 1}`}
-            className="background-selector-image"
-          />
-                <div className="background-selector-radios">
-        {backgrounds.map((_, idx) => (
-          <img
-            key={idx}
-            src={idx === currentIndex ? radioBtnActive : radioBtn}
-            alt={idx === currentIndex ? 'Выбрано' : 'Не выбрано'}
-            className="background-selector-radio"
-            onClick={() => setCurrentIndex(idx)}
-          />
-        ))}
-      </div>
+      <div className="conteiner-selecct">
+        <div className="background-selector-mask-wrapper">
+          <img src={fonMask} alt="Маска" className="background-selector-mask" />
+          <div className="background-selector-image-wrapper">
+            <img
+              src={backgrounds[currentIndex].url}
+              alt={`Фон ${currentIndex + 1}`}
+              className="background-selector-image"
+            />
+            <div className="background-selector-radios">
+              {backgrounds.map((_, idx) => (
+                <img
+                  key={idx}
+                  src={idx === currentIndex ? radioBtnActive : radioBtn}
+                  alt={idx === currentIndex ? "Выбрано" : "Не выбрано"}
+                  className="background-selector-radio"
+                  onClick={() => setCurrentIndex(idx)}
+                />
+              ))}
+            </div>
+          </div>
         </div>
-        
-      </div>
         <img src={text} alt="text" className="background-selector-desc" />
-      <button
-        onClick={() => onSelect(backgrounds[currentIndex].url, backgrounds[currentIndex].id)}
-        className="background-selector-btn"
-      >
-        <img src={selectBtn} alt="Подтвердить выбор" />
-      </button>
+        <button
+          onClick={() =>
+            onSelect(
+              backgrounds[currentIndex].url,
+              backgrounds[currentIndex].id,
+            )
+          }
+          className="background-selector-btn"
+        >
+          <img src={selectBtn} alt="Подтвердить выбор" />
+        </button>
       </div>
     </>
   );
-} 
+}
