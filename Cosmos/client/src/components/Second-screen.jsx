a4eb067547e3336c0803f037d684bab193fa18b2
annapirz <fanworld991@gmail.com>
2025-08-15 13:37:16 +0300

    fix: bug

--- Cosmos/client/src/components/Second-screen.jsx
+++ Cosmos/client/src/components/Second-screen.jsx
@@ -1,10 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import BlueFrame from '../ui/BlueFrame.jsx';
 import FaidIn from '../ui/FaidIn.jsx';
 import WebcamCapture from './WebcamCapture.jsx';
+import { useCountdown } from '../context/CountdownContext.jsx';
 
 const SecondScreen = () => {
     const webcamRef = useRef(null);
+    const { resetState } = useCountdown();
+
+    useEffect(() => {
+        resetState();
+    }, []);
 
     return (
         <div className="relative w-full h-screen overflow-hidden">
