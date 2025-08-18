65ee6383a8c0dc71449a90091a4e38d75b477932
annapirz <fanworld991@gmail.com>
2025-08-18 14:53:40 +0300

    feat: cosmos add syspence

--- Cosmos/client/src/App.jsx
+++ Cosmos/client/src/App.jsx
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { CountdownProvider } from './context/CountdownContext.jsx';
 import ExitTransition from "./ui/ExitTransition.jsx";
 import EnterTransition from "./ui/EnterTransition.jsx";
-import Greeting from './components/Greeting.jsx';
-import SecondScreen from './components/Second-screen.jsx';
-import ThreeScreen from './components/ThreeScreen.jsx';
+const Greeting = lazy(() => import('./components/Greeting.jsx'));
+const SecondScreen = lazy(() => import('./components/Second-screen.jsx'));
+const ThreeScreen = lazy(() => import('./components/ThreeScreen.jsx'));
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
-      <ExitTransition>
-        <Greeting />
-      </ExitTransition>
+        <Suspense fallback={<div className="bg-[url(/kosmosFon.png)] h-screen w-screen bg-center bg-cover" /> }>
+        <ExitTransition>
+          <Greeting />
+          </ExitTransition>
+
+        </Suspense>
     ),
   },
   {
     path: "/second",
-    element: <SecondScreen />,
+    element: (
+      <Suspense fallback={<div className="bg-[url(/kosmosFon.png)] h-screen w-screen bg-center bg-cover" /> }>
+        <SecondScreen />
+      </Suspense>
+    ),
   },
   {
     path: "/three",
     element: (
       <EnterTransition>
-        <ThreeScreen />
+        <Suspense fallback={<div className="bg-[url(/kosmosFon.png)] h-screen w-screen bg-center bg-cover" /> }>
+          <ThreeScreen />
+        </Suspense>
       </EnterTransition>
     ),
   },
@@ -33,6 +42,7 @@ const router = createBrowserRouter([
 function App() {
   return (
     <CountdownProvider>
+      <div className="fixed inset-0 -z-10 bg-[url(/kosmosFon.png)] bg-center bg-cover pointer-events-none" />
       <RouterProvider router={router} />
     </CountdownProvider>
   );
