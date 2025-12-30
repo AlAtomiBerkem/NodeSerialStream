0d0591cf9bc4a1480f1bfa376273e60217005975
annapirz <fanworld991@gmail.com>
2025-12-30 10:01:32 +0300

    feat: update mainpage breefing

--- Briefing/src/pages/MainPage/ui/MainPage.tsx
+++ Briefing/src/pages/MainPage/ui/MainPage.tsx
@@ -1,34 +1,44 @@
-import cls from './MainPage.module.scss'
-import { classNames } from "shared/lib";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { routePath, RouteConfig } from "shared/config/routeConfig";
 import { BackgroundScreen } from "widgets/BackgroundScreen";
-import { NavCard } from "./NavCard/NavCard";
+import cls from './MainPage.module.scss'
+import { classNames } from "shared/lib";
+
+// Import Assets
+import InstructionHeader from "shared/assets/instruction.svg";
+import InstructionBtn from "shared/assets/instructionPageBtn.svg";
+import RouteBtn from "shared/assets/RoutMapPageBtn.svg";
+import Logo from "shared/assets/Label.svg";
 
 interface MainPageProps {
     className?: string;
 }
 
 export const MainPage = ({ className }: MainPageProps) => {
-    const navigate = useNavigate();
-
-    // Text content from Figma design
-    // Node 219:2048 (Instructions)
-    // Node 219:2053 (Exhibition)
-
     return (
         <BackgroundScreen className={classNames(cls.MainPage, {}, [className])}>
-            <div className={cls.cardContainer}>
-                <NavCard
-                    title="ИНСТРУКТАЖ"
-                    description="Перед входом в фюзеляж Ту-144 ознакомься с инструкцией и маршрутом экспозиции"
-                    onClick={() => navigate(routePath[RouteConfig.INSTRUCTIONS])}
-                />
-                <NavCard
-                    title="МАРШРУТ ЭКСПОЗИЦИИ"
-                    description="Схема осмотра фюзеляжа с ключевыми точками и пояснениями"
-                    onClick={() => navigate(routePath[RouteConfig.EXHIBITIONROUTE])}
-                />
+            <div className={cls.container}>
+                <div className={cls.header}>
+                    <img src={InstructionHeader} className={cls.headerMain} alt="Инструктаж" />
+                </div>
+
+                <p className={cls.subtitle}>
+                    Перед входом в фюзеляж Ту-144 ознакомься с инструкцией <br />
+                    и маршрутом экспозиции
+                </p>
+
+                <div className={cls.btnContainer}>
+                    <Link to={routePath[RouteConfig.INSTRUCTIONS]} className={cls.link}>
+                        <img src={InstructionBtn} className={cls.btnImg} alt="Инструкция" />
+                    </Link>
+                    <Link to={routePath[RouteConfig.EXHIBITIONROUTE]} className={cls.link}>
+                        <img src={RouteBtn} className={cls.btnImg} alt="Маршрут экспозиции" />
+                    </Link>
+                </div>
+
+                <div className={cls.footer}>
+                    <img src={Logo} className={cls.logo} alt="Ту-144 Музей" />
+                </div>
             </div>
         </BackgroundScreen>
     );
